213c9c97a20b1f4ca7658126badae27428de865f
Péter Molnár <xodax9898@gmail.com>
2021-12-08 23:56:33 +0100

    Basic user authentication without a database

--- signin.py
+++ signin.py
@@ -3,7 +3,23 @@ from kivy.uix.boxlayout import BoxLayout
 
 
 class SignInWindow(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def validate_user(self):
+        user = self.ids.username_field
+        pwd = self.ids.pwd_field
+
+        user_name = user.text
+        password = pwd.text
+        info = self.ids.info
+
+        if user_name == '' or password == '':
+            info.text = '[color=#FF0000]Username and/or password is required[/color]'
+        elif user_name == 'admin' and password == "admin":
+            info.text = '[color=#00FF00]Logged in successfully![/color]'
+        else:
+            info.text = '[color=#FF0000]Invalid username and/ or password! [/color]'
 
 
 class SignInApp(App):
